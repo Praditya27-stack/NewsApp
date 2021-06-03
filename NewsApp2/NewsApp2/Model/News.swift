38485015572e302470e250195c93744a3e03f674
Praditya27-stack <68719080+Praditya27-stack@users.noreply.github.com>
2021-06-03 21:46:35 +0700

    Add files via upload

---
+++ NewsApp2/NewsApp2/Model/News.swift
@@ -0,0 +1,14 @@
+//
+//  News.swift
+//  NewsApp2
+//
+//  Created by Praditya Abyan on 08/02/21.
+//
+
+import Foundation
+struct News : Identifiable{
+    var id = UUID()
+    var title : String
+    var image : String
+    var description : String
+}
---
+++ NewsApp2/NewsApp2/ViewModel/NewsApi.swift
@@ -0,0 +1,42 @@
+//
+//  NewsApi.swift
+//  NewsApp2
+//
+//  Created by Praditya Abyan on 08/02/21.
+//
+
+import Foundation
+import Combine
+import SwiftyJSON
+
+class NewsApi : ObservableObject{
+    @Published var data = [News]()
+    
+    init(){
+        let url =
+        "https://newsapi.org/v2/top-headlines?country=id&category=science&apiKey=028e70b349484a649171195ca796b7ba"
+        
+        let session = URLSession(configuration: .default)
+        
+        session.dataTask(with: URL(string: url)!){(data , _, err)in
+            if err != nil{
+                print((err?.localizedDescription)!)
+                return
+            }
+            
+            let json = try! JSON(data: data!)
+            let items = json["articles"].array!
+            
+            for i in items{
+                let title = i["title"].stringValue
+                let description = i["description"].stringValue
+                let imurl = i["urlToImage"].stringValue
+                
+                DispatchQueue.main.async {
+                    self.data.append(News(title: title, image: imurl, description: description))
+                }
+            }
+        }
+        .resume()
+    }
+}
